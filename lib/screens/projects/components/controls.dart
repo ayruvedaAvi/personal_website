02c7980d92805e35db16a4f008a253a03ef20a6c
Abhishek Neupane <neupaneabhishek13@gmail.com>
2024-07-30 16:09:01 +0545

    added the basic layout and key handling in projects screen

---
+++ lib/screens/projects/components/controls.dart
@@ -0,0 +1,91 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Controls extends StatelessWidget {
+  final Function upPressed;
+  final Function downPressed;
+  final Function leftPressed;
+  final Function rightPressed;
+
+  const Controls({
+    super.key,
+    required this.upPressed,
+    required this.downPressed,
+    required this.leftPressed,
+    required this.rightPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.25,
+      width: MediaQuery.of(context).size.width * 0.2,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          Text(
+            "You can also use arrow keys to move.",
+            style: TextStyle(
+              color: Colors.lightBlue,
+              fontSize: 10,
+              fontFamily: GoogleFonts.robotoMono().fontFamily,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              const SizedBox(
+                width: 40,
+              ),
+              IconButton(
+                onPressed: () => upPressed(),
+                icon: SvgPicture.asset(
+                  'assets/icons/arrow-up.svg',
+                  colorFilter:
+                      const ColorFilter.mode(Colors.lightBlue, BlendMode.srcIn),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () => leftPressed(),
+                icon: SvgPicture.asset(
+                  'assets/icons/arrow-left.svg',
+                  colorFilter:
+                      const ColorFilter.mode(Colors.lightBlue, BlendMode.srcIn),
+                ),
+              ),
+              IconButton(
+                onPressed: () => downPressed(),
+                icon: SvgPicture.asset(
+                  'assets/icons/arrow-down.svg',
+                  colorFilter:
+                      const ColorFilter.mode(Colors.lightBlue, BlendMode.srcIn),
+                ),
+              ),
+              IconButton(
+                onPressed: () => rightPressed(),
+                icon: SvgPicture.asset(
+                  'assets/icons/arrow-right.svg',
+                  colorFilter:
+                      const ColorFilter.mode(Colors.lightBlue, BlendMode.srcIn),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/projects/projects_screen.dart
+++ lib/screens/projects/projects_screen.dart
@@ -1,23 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../core/constants/colors.dart';
-import '../../components/custom_appbar.dart';
+import 'components/controls.dart';
 
-class ProjectsScreen extends StatelessWidget {
+class ProjectsScreen extends StatefulWidget {
   const ProjectsScreen({super.key});
+
+  @override
+  State<ProjectsScreen> createState() => _ProjectsScreenState();
+}
+
+class _ProjectsScreenState extends State<ProjectsScreen> {
+  String text = "Projects Screen";
+
+  upPressed() {
+    setState(() {
+      text = "Up Pressed";
+    });
+  }
+
+  void downPressed() {
+    setState(() {
+      text = "Down Pressed";
+    });
+  }
+
+  void leftPressed() {
+    setState(() {
+      text = "Left Pressed";
+    });
+  }
+
+  void rightPressed() {
+    setState(() {
+      text = "Right Pressed";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: baseColor,
-      body: Stack(
-        children: [
-          const Center(
-            child: Text("Projects Screen"),
-          ),
-          CustomAppbar(selectedPage: "Projects"),
-        ],
-      ),
+      body: SafeArea(
+          child: KeyboardListener(
+            focusNode: FocusNode(),
+            onKeyEvent: (value) {
+              if (value.logicalKey == LogicalKeyboardKey.arrowUp) {
+                setState(() {
+                  text = "Up Pressed";
+                });
+                upPressed();
+              } else if (value.logicalKey == LogicalKeyboardKey.arrowDown) {
+                downPressed();
+              } else if (value.logicalKey == LogicalKeyboardKey.arrowLeft) {
+                leftPressed();
+              } else if (value.logicalKey == LogicalKeyboardKey.arrowRight) {
+                rightPressed();
+              }
+            },
+            child: Stack(
+              children: [
+                Center(
+                  child: Text(
+                    text,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                Controls(
+                  downPressed: downPressed,
+                  leftPressed: leftPressed,
+                  rightPressed: rightPressed,
+                  upPressed: upPressed,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
